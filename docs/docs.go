374f3ab2b4a06301364154dcce1b968b0f22330c
maheswaradevo <pundadevo21@gmail.com>
2023-10-22 23:14:03 +0800

    fix: add origin

--- docs/docs.go
+++ docs/docs.go
@@ -819,7 +819,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080/api/v1",
+	Host:             "xxx-backend.deveureka.com",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Wonderworxs Dashboard API",
